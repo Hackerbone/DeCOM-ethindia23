4dd4c47129fa4de28445087652844b3aa0f93511
shero4 <goyal.dhruva@gmail.com>
2023-12-08 23:11:31 +0530

    added isVendor in VendorFactory

--- contracts/VendorFactory.sol
+++ contracts/VendorFactory.sol
@@ -16,14 +16,21 @@ contract VendorFactory {
     // Set the deployment cost for creating a new Vendor contract
     // uint256 public constant VENDOR_CREATION_FEE = 1 wei;
 
-    event VendorCreated(address indexed vendorAddress, string name, string logo);
+    event VendorCreated(
+        address indexed vendorAddress,
+        string name,
+        string logo
+    );
 
     constructor() {
         // The account deploying the contract becomes the owner
         owner = msg.sender;
     }
 
-    function createVendorContract(string memory _name, string memory _logo) public {
+    function createVendorContract(
+        string memory _name,
+        string memory _logo
+    ) public {
         // Require the sender to send 0.1 ETH
         //require(msg.value == VENDOR_CREATION_FEE, "Must pay 0.1 ETH to create a vendor");
 
@@ -41,5 +48,15 @@ contract VendorFactory {
     function withdraw() public {
         require(msg.sender == owner, "Only the owner can withdraw");
         payable(owner).transfer(address(this).balance);
+    }
+
+    // Function that takes a address and returns if that address is a vendor or not boolean
+    function isVendor(address _vendorAddress) public view returns (bool) {
+        for (uint i = 0; i < vendors.length; i++) {
+            if (vendors[i].vendorAddress == _vendorAddress) {
+                return true;
+            }
+        }
+        return false;
     }
 }
