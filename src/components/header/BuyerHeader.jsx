e7fda4f7d170181bdbe7257e177c8f6536154b53
Aditya Mohan <aditya530026@gmail.com>
2023-12-09 08:14:50 +0530

    login on buyer side

--- src/components/header/BuyerHeader.jsx
+++ src/components/header/BuyerHeader.jsx
@@ -9,12 +9,46 @@ import ProfileDropdown from './ProfileDropdown'
 import { showConfirm } from 'components/modals/ConfirmModal'
 import { useNavigate } from 'react-router-dom'
 import { logout } from 'store/user.slice'
+import {
+    setIsConnected,
+    setStoreId,
+    setUserType,
+    setWalletAddress,
+  } from "store/user.slice";
+  import Web3 from "web3";
+import { checkVendor } from 'services/vendorfactory.service'
+
 
 const BuyerHeader = () => {
     const { storeId, walletAddress } = useSelector((state) => state.user);
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const handleSafeLogin = async () => {
+        if (window.ethereum) {
+          const web3 = new Web3(window.ethereum);
+          const accounts = await web3.eth.requestAccounts();
+    
+          const userData = await checkVendor(accounts[0]);
+    
+          if (!userData) {
+            dispatch(setIsConnected(true));
+            dispatch(setWalletAddress(accounts[0]));
+            dispatch(setUserType("user"));
+            navigate("/create-store");
+            return;
+          } else {
+            // Dispatch actions to update the Redux store
+            dispatch(setWalletAddress(userData.vendorWalletAddress));
+            dispatch(setIsConnected(true));
+            dispatch(setUserType("vendor"));
+            dispatch(setStoreId(userData.vendorAddress));
+          }
+        } else {
+          console.error("MetaMask is not installed");
+        }
+      };
 
     const handleLogout = () => {
         showConfirm({
@@ -32,7 +66,7 @@ const BuyerHeader = () => {
             <Image src={fulllogo} preview={false} className={styles.loginPageLogo} />
             <div className={styles.navbarItems}>
                 {!walletAddress ?
-                    <PrimaryButton className={styles.loginButton}>Login</PrimaryButton> :
+                    <PrimaryButton onClick={handleSafeLogin} className={styles.loginButton}>Login</PrimaryButton> :
                     <Dropdown trigger={["click"]} dropdownRender={() => <ProfileDropdown handleLogout={handleLogout} address={walletAddress} />}>
                         <PrimaryButton className={styles.accountButton} icon={<MdOutlineAccountCircle className={styles.accountIcon} />} />
                     </Dropdown>}
